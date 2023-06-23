244cefb545db94c3cfc948a80f7f56753c9297af
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-23 13:47:56 +0530

    submitting code-Fri Jun 23 2023 13:47:55 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
-
-import React from "react";
-import './../styles/App.css';
-
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Child from "./Child";
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [prices, setPrices] = useState([]);
+
+  const deleteItem = (index) => {
+    setItems(
+      items.filter((v, i) => {
+        return i !== index;
+      })
+    );
+    setPrices(
+      prices.filter((v, i) => {
+        return i !== index;
+      })
+    );
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      <h1>Parent Component</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setItems([...items, e.target.item.value]);
+          setPrices([...prices, e.target.price.value]);
+        }}
+      >
+        <label htmlFor="itemName">Item Name</label>
+        <input type="text" id="itemName" name="item" />
+        <label htmlFor="itemPrice">Item Price</label>
+        <input type="number" id="itemPrice" name="price" />
+        <button>Add Item</button>
+      </form>
+      <Child item={items} price={prices} func={deleteItem}/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Child.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Child = (props) => {
+  return (
+    <>
+      <h1>Child Component</h1>
+      <ul>
+        {/* <li> <button onClick={}>Remove</button></li> */}
+        {props.item.map((value, i) => {
+          return (
+            <li key={i + 1}>
+              {value} - ${props.price[i]}
+              <button
+                onClick={(i) => {
+                  props.func(i);
+                  console.log("gghgh")
+                }}
+              >
+                Remove Item
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default Child;
